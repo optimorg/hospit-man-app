1cbe62d8eac97200e63e8e0e738dbd1f9e32bb35
morgREEN <morgreen25@gmail.com>
2023-11-27 18:43:57 -0500

    Dashboard initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EnvironmentComponent } from './src/environments/environment/environment.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DoctorComponent } from './components/dashboard/doctor/doctor.component';
+import { PatientComponent } from './components/dashboard/patient/patient.component';
+import { SideBarComponent } from './components/dashboard/side-bar/side-bar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EnvironmentComponent
+    EnvironmentComponent,
+    DashboardComponent,
+    DoctorComponent,
+    PatientComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
