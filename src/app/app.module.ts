9b821a8113eaed1a54901cd185b174b6c00f1ba4
morgREEN <morgreen25@gmail.com>
2023-11-27 18:33:31 -0500

    Set up environment with firebase,material,bootstrap, etc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EnvironmentComponent } from './src/environments/environment/environment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnvironmentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
