736d84b59a5c97d111dba041b75c34a4a35e11de
morgREEN <morgreen25@gmail.com>
2023-11-27 20:11:53 -0500

    addDoctor  skeleton

--- src/app/components/dashboard/doctor/doctor.component.ts
+++ src/app/components/dashboard/doctor/doctor.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-doctor',
   templateUrl: './doctor.component.html',
   styleUrls: ['./doctor.component.css']
 })
-export class DoctorComponent {
+export class DoctorComponent implements OnInit {
+  
+  constructor() { }
 
+  ngOnInit(): void {
+    
+  }
+
+  addDoctor() {
+    
+  }
 }
